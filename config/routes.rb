5671e57167affabaa213224f595de6bc7f6e35e7
m7madmagdy <mohamedmagdysaber@gmail.com>
2023-04-12 15:08:51 +0200

    Created devise for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   get 'home/about'
-  root "posts#index"
+  root 'posts#index'
 
   resources :posts
+  devise_for :users
+  devise_scope :user do
+    get '/users', to: 'devise/registrations#new'
+    get '/users/password', to: 'devise/passwords#new'
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+  end
 end
