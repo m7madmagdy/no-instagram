e041dbf8919b3599d85858c16501ed6ec12afbf6
Mohamed Magdy <mohamedmagdysaber@gmail.com>
2023-05-29 03:22:40 +0300

    enhanced-app

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,19 +1,20 @@
 class Post < ApplicationRecord
-    before_create :randomize_id
+  before_create :randomize_id
 
-    validates :title, presence: true, length: { minimum: 5, maximum: 100}
-    validates :description, presence: true, length: { minimum: 5, maximum: 500}
-    validates :keywords, presence: true, length: { minimum: 5, maximum: 100}
+  validates :title, presence: true, length: { minimum: 5, maximum: 100 }
+  validates :description, presence: true, length: { minimum: 5, maximum: 500 }
+  validates :keywords, presence: true, length: { minimum: 5, maximum: 100 }
 
-    has_many_attached :images
-    belongs_to :user
-    has_many :comments
-    has_many :likes
+  belongs_to :user
+  has_many_attached :images, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
-    private
-    def randomize_id
-        begin
-            self.id = SecureRandom.random_number(1_000_000_000)
-        end while User.where(id: self.id).exists?
-    end
+  private
+
+  def randomize_id
+    begin
+      self.id = SecureRandom.random_number(1_000_000_000)
+    end while User.where(id: self.id).exists?
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :posts
-  has_many :comments
-  has_many :likes
-  has_one_attached :avatar
+  has_many :posts, foreign_key: :user_id, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_one_attached :avatar, dependent: :destroy
 
   private
 
