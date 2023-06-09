6ac487e5c0e1b86aee2fbb64e252fba3d20eec06
Mohamed Magdy <mohamedmagdysaber@gmail.com>
2023-06-09 04:02:40 +0300

    Covered services

--- spec/repositories/attachment_repo_spec.rb
+++ spec/repositories/attachment_repo_spec.rb
@@ -1,21 +1,23 @@
 RSpec.describe AttachmentRepo, type: :repo do
-    let(:article) { FactoryBot.create(:article, :with_user) }
-    let(:attachment) { FactoryBot.create(:attachment, :with_article) }
-    let(:attachment_repo) { AttachmentRepo.new(article, attachment.raw_response, attachment.resource_id, attachment.resource_type) }
-  
-    describe "#create_attachment" do
-      it "creates a new attachment and returns its resource URL" do
-        expect {
-          attachment_repo.create_attachment
-        }.to change { article.attachments.count }.by(1)
-  
-        last_attachment = article.attachments.last
-        expect(last_attachment.raw_response).to eq(attachment.raw_response)
-        expect(last_attachment.resource_id).to eq(attachment.resource_id)
-        expect(last_attachment.resource_type).to eq(attachment.resource_type)
-  
-        resource_url = "#{Attachment::BASE_URL}#{last_attachment.resource_id}.#{last_attachment.resource_type.split("/").last}"
-        expect(attachment.resource_url).to eq(resource_url)
-      end
+  let(:post) { FactoryBot.create(:post, :with_user) }
+  let(:attachment) { FactoryBot.create(:attachment, :with_post) }
+  let(:attachment_repo) { 
+    AttachmentRepo.new(post, attachment.raw_response, attachment.resource_id, 
+    attachment.resource_type, attachment.resource_url)
+  }
+
+  describe "#create_attachment" do
+    it "creates a new attachment and returns its resource URL" do
+      expect {
+        attachment_repo.create_attachment
+      }.to change { post.attachments.count }.by(1)
+
+      last_attachment = post.attachments.last
+      expect(last_attachment.raw_response).to eq(attachment.raw_response)
+      expect(last_attachment.resource_id).to eq(attachment.resource_id)
+      expect(last_attachment.resource_type).to eq(attachment.resource_type)
+      expect(last_attachment.resource_url).to eq(attachment.resource_url)
+      expect(attachment.resource_url).to eq(attachment.resource_url)
     end
   end
+end
