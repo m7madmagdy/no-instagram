c012a06f00e0a48a00daa942e3336100ef0eeda0
Mohamed Magdy <mohamedmagdysaber@gmail.com>
2023-06-11 03:01:19 +0300

    Covered models

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,13 @@
-RSpec.describe 'User' do
-  context 'assocciations' do
-    it 'has many likes' do
-      t = User.reflect_on_association(:likes)
-      expect(t.macro).to eq(:has_many)
-    end
+RSpec.describe User, type: :model do
+  describe 'associations' do
+    it { should have_many(:posts).dependent(:destroy) }
+    it { should have_many(:comments).dependent(:destroy) }
+    it { should have_many(:likes).dependent(:destroy) }
+    it { should have_many(:avatars).dependent(:destroy) }
   end
+
+  describe 'nested attributes' do
+    it { should accept_nested_attributes_for(:avatars) }
+  end
+
 end
